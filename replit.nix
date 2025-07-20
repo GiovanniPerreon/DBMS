76a1b05a61edc501b1db1b60ddaba1a8d2133a8c
GiovanniPerreon <giovanni.perreon@studio.unibo.it>
2025-07-20 18:15:40 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,6 +1,10 @@
 { pkgs }: {
   deps = [
-    pkgs.python311
+    pkgs.nodejs_23
+    pkgs.nodePackages.npm
+    pkgs.python3Full
     pkgs.ffmpeg
+    pkgs.python3Packages.pip
+    pkgs.python3Packages.soundfile
   ];
 }
